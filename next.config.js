c9a34910f838e4c9d57c29b7e7fef9cff7e96b66
iTyphoons <hon.yang88@hotmail.com>
2020-12-22 20:27:15 +0000

    Bump node-notifier from 8.0.0 to 8.0.1

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-const { withPlugins } = require("next-compose-plugins");
-const optimizedImages = require("next-optimized-images");
-
-module.exports = withPlugins([[optimizedImages, { optimizeImages: false }]], {
-	dir: "src",
-	distDir: "../build",
-	cssModules: true,
-});
